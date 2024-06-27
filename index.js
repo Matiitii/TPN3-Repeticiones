e27673f2563fa877a8b6ac3674819a19d7471608
Matii <166016459+Matiitii@users.noreply.github.com>
2024-06-27 18:06:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//actividad 1 
+
+for (let index = 10; index >= 1; index= index - 1) {
+    console.log (index)
+}
+
+
+//actividad 2
+
+
+
+
+for (let index = 15; index >= 5; index= index - 1) {
+    console.log (index)
+}
+
+
+
+
+//actividad 3
+
+
+for (let index = 1; index <= 100; index= index + 2) {
+    console.log (index)
+}
+
+//actividad 4 
+
+for (let index = 0; index <= 10; index= index + 1) {
+   console.log ( '7x' + index + ' = ' + (index * 7)  )
+    
+}
